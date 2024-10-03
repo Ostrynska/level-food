9295d690cc7e586afb2797ab955981c115d9b65c
Ostrynska <kateriinag@gmail.com>
2024-10-03 12:41:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/level-food',
-  assetPrefix: '/level-food/',
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
